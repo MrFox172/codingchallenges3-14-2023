f578f930440f9e277a6f5d3cb59bd15e85959981
Zackery Perski <zackeryperski@gmail.com>
2023-03-14 10:34:45 -0400

    Updated Python version.

--- Python/Adder.py
+++ Python/Adder.py
@@ -1,10 +1,10 @@
-def adder(a,b):
+def addition(a,b):
     return a+b
 
 def main():
-    print(adder(3,2))
-    print(adder(-3,-6))
-    print(adder(7,3))
+    print(addition(3,2))
+    print(addition(-3,-6))
+    print(addition(7,3))
 
 if __name__ == "__main__":
     main()
